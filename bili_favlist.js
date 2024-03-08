0dcdc541812c4a80480a513e1d4586c02ef6a9d1
firavoyage <devvhy@zohomail.cn>
2024-03-08 20:58:43 +0800

    .

--- bili_favlist.js
+++
@@ -1,7 +0,0 @@
-// snippet on page https://space.bilibili.com/1156993272/favlist?fid=2801574772&ftype=create
-// created at jan30 2024
-
-var a=document.getElementById("fav-createdList-container").innerHTML
-var l = a.search(/1156993272/i)
-console.log(a.search("1156993272"))
-

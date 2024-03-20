f2469baa413e192f038b78b44a50c10b4ea5e922
firavoyage <devvhy@zohomail.cn>
2024-03-20 19:04:35 +0800

    .

---
+++ luogu/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <climits>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int a=1;
+    int b=2;
+    cout<<a ++-+-++ b;
+
+}
